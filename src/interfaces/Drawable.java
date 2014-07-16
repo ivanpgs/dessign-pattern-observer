8fab8e83317727aff66dbcb5d0d6e9188d32b826
Ivan <ivanpgs@gmail.com>
2014-07-16 03:15:06 +0200

    Drawable.java
    Interface that define the operation draw within a graphic

---
+++ src/interfaces/Drawable.java
@@ -0,0 +1,14 @@
+package interfaces;
+import java.awt.Graphics;
+
+/**
+ * @author Ivan Poza
+ * 
+ * Interface that define the operation draw within a graphic
+ *
+ */
+public interface Drawable {
+
+	public void draw (Graphics c);
+
+}
